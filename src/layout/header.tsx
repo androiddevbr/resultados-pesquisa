97f4d17a25f76462b93c468648e6f904961aaa63
Ana Coimbra <ana.coimbra.gomes@gmail.com>
2020-08-06 22:39:13 -0300

    add metodology and dataset to header buttons

--- src/layout/header.tsx
+++ src/layout/header.tsx
@@ -1,7 +1,7 @@
 import { Link as GatsbyLink } from 'gatsby'
 import React, { FC } from 'react'
-import { AppBar, Toolbar, Button, Avatar, ButtonBase, Tabs, Tab, Modal } from '@material-ui/core'
-import { makeStyles, useTheme } from '@material-ui/styles'
+import { AppBar, Toolbar, Button, Avatar, ButtonBase, Tabs, Tab, Modal, Typography } from '@material-ui/core'
+import { createStyles, makeStyles, Theme, useTheme } from '@material-ui/styles'
 import useMediaQuery from '@material-ui/core/useMediaQuery';
 import logo from './../images/logo.png'
 import data from '../../static/data.json';
@@ -13,21 +13,38 @@ const a11yProps = (index: any) => {
   };
 }
 
-const useStyles = makeStyles({
-  toolbar: {
-    backgroundColor: '#424242',
-    justifyContent: 'space-between',
-    color: '#fff'
-  },
-  tabs: {
-    backgroundColor: '#424242',
-    justifyContent: 'space-between',
-    color: '#fff'
-  },
-  link: {
-    textDecoration: 'none',
-  },
-})
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    toolbar: {
+      backgroundColor: '#424242',
+      justifyContent: 'space-between',
+      color: '#fff'
+    },
+    tabs: {
+      backgroundColor: '#424242',
+      justifyContent: 'space-between',
+      color: '#fff'
+    },
+    link: {
+      textDecoration: 'none',
+    },
+    modal: {
+      display: 'flex',
+      padding: theme.spacing(1),
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    paper: {
+      maxWidth: 800,
+      backgroundColor: theme.palette.background.paper,
+      border: 'none',
+      borderRadius: 6,
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+      outline: 'none'
+    },
+  })
+);
 
 export interface HeaderProps {
   siteTitle?: string
@@ -57,9 +74,10 @@ const Header: FC<HeaderProps> = ({ siteTitle = '', onToggleTheme, onChangeTab })
     onChangeTab(newValue);
   };
 
+  const rootRef = React.useRef<HTMLDivElement>(null);
 
   return (
-    <AppBar component="header" position="static">
+    <AppBar component="header" position="static" ref={rootRef}>
       <Toolbar className={classes.toolbar}>
         <ButtonBase
           to="/"
@@ -69,9 +87,11 @@ const Header: FC<HeaderProps> = ({ siteTitle = '', onToggleTheme, onChangeTab })
           <Avatar alt="Android Dev BR" src={logo} />
         </ButtonBase>
         <div>
-          <Button color="primary" disabled >
-            Dados brutos (em breve)
-        </Button>
+          <ButtonBase component={GatsbyLink} to="https://github.com/androiddevbr/resultados-pesquisa/data"  >
+            <Button color="inherit">
+              Dados brutos
+            </Button>
+          </ButtonBase>
           <Button color="inherit" onClick={handleOpen}>
             Metodologia
         </Button>
@@ -88,9 +108,32 @@ const Header: FC<HeaderProps> = ({ siteTitle = '', onToggleTheme, onChangeTab })
       <Modal
         open={open}
         onClose={handleClose}
+        className={classes.modal}
         aria-labelledby="simple-modal-title"
         aria-describedby="simple-modal-description">
-        <div style={{ width: 400, height: 100, margin: 'auto', backgroundColor: `#fff`, padding: 4 }}>TODO</div>
+        <div className={classes.paper}>
+          <Typography variant="h2" component="h2" >Metodologia</Typography>
+          <div>
+            <Typography paragraph>
+              Entre os dias 1 de Junho e 15 de Julho de 2020 abrimos e divulgamos um formulário com toda a comunidade Android Dev BR que tem um foco em desenvolvimento android. Durante esse período 323 pessoas responderam este formulário que possui diversas perguntas:
+          </Typography>
+
+            <ol>
+              <li>gerais sobre dados demográficos: escolaridade, faixa etária, etc.</li>
+              <li>dados sobre o contexto trabalhista: regime de trabalho, tamanho da empresa, etc.</li>
+              <li>dados mais específicos relacionados ao trabalho com desenvolvimento android: cargo, tamanho do time, etc.</li>
+              <li>dados específicos sobre trabalho em empresas no Brasil: localização, faixa salarial, benefícios, etc.</li>
+              <li>dados específicos sobre trabalho no exterior: localização, modeda, faixa salarial, etc.</li>
+              <li>dados sobre trabalho como freelancer: valor por hora, tipos de projetos, etc.</li>
+            </ol>
+
+            <Typography paragraph>
+              Aproveitamos a oportunidade para também buscar feedbacks sobre a atuação e impacto da comunidade na vida e carreira profissional das pessoas participantes.
+            <br /><br />
+            É possível ver mais informações sobre os resultados da pesquisa no <a className={classes.link} style={{ color: theme.palette.text.secondary }} target="_blank" href="https://github.com/androiddevbr/resultados-pesquisa/data">Github</a>
+            </Typography>
+          </div>
+        </div>
       </Modal>
     </AppBar>
   )
